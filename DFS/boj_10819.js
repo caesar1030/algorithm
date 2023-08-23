724fa989fffadde5497ac328522225492b534d35
zlx454545 <zlx454545@gmail.com>
2023-08-23 19:47:14 +0900

    boj 10819

---
+++ DFS/boj_10819.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+const n = +input[0];
+const arr = input[1].split(' ').map(Number);
+let ans = 0;
+
+const permutaition = (start) => {
+  if (start === n) {
+    const temp = arr.reduce((acc, cur, idx) => {
+      if (idx === n - 1) return acc;
+
+      return acc + Math.abs(arr[idx] - arr[idx + 1]);
+    }, 0);
+
+    ans = Math.max(ans, temp);
+  }
+
+  for (let i = start; i < n; i++) {
+    [arr[i], arr[start]] = [arr[start], arr[i]];
+    permutaition(start + 1);
+    [arr[i], arr[start]] = [arr[start], arr[i]];
+  }
+};
+
+const main = () => {
+  permutaition(0);
+  console.log(ans);
+};
+
+main();
