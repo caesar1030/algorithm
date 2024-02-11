27b63185667936a84a89c6f540a47b0b70aec9a5
zlx454545 <zlx454545@gmail.com>
2024-02-12 04:58:35 +0900

    boj 1904

---
+++ Dynamic/boj_1904.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+let line = 0;
+
+const n = +input[line++];
+
+const dp = new Array(n + 1).fill(0);
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+}
+
+console.log(dp[n]);
