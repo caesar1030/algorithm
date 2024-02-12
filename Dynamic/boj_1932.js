dfc04db93cf5f1c581cef12152ed25df1ae1ce8f
zlx454545 <zlx454545@gmail.com>
2024-02-12 17:28:44 +0900

    boj 1932

---
+++ Dynamic/boj_1932.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+let line = 0;
+
+const n = +input[line++];
+const arr = new Array(n);
+const dp = [...new Array(n)].map(() => new Array(n).fill(0));
+
+for (let i = 0; i < n; i++) {
+  arr[i] = input[line++].split(' ').map(Number);
+}
+
+dp[0][0] = arr[0][0];
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j <= i; j++) {
+    if (j === 0) dp[i][j] = dp[i - 1][j] + arr[i][j];
+    else if (j === i) dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+    else dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+  }
+}
+
+console.log(Math.max(...dp[n - 1]));
