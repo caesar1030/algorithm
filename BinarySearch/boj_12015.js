705ffa287fa0b3e016ba00798a34dfca896ccede
zlx454545 <zlx454545@gmail.com>
2024-03-26 03:40:43 +0900

    boj 12015

---
+++ BinarySearch/boj_12015.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+let line = 0;
+
+const n = +input[line++];
+const arr = input[line++].split(' ').map(Number);
+
+const lb = (array, target) => {
+  let l = 0;
+  let r = array.length;
+
+  while (l < r) {
+    const mid = Math.floor((l + r) / 2);
+    if (array[mid] >= target) r = mid;
+    else l = mid + 1;
+  }
+
+  return r;
+};
+
+const ans = [0];
+arr.forEach((el) => {
+  if (el > ans.at(-1)) ans.push(el);
+  else {
+    const idx = lb(ans, el);
+    ans[idx] = el;
+  }
+});
+
+console.log(ans.length - 1);
