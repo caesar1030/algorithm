5102ac75a848f5f1da4531ba466381b9a91623bc
zlx454545 <zlx454545@gmail.com>
2024-05-17 10:50:51 +0900

    boj 11047

--- Greedy/boj_11047.js
+++ Greedy/boj_11047.js
@@ -3,26 +3,21 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 let line = 0;
 
-const [numMoneyValues, toMakeValue] = input[line++].split(' ').map(Number);
-const moneyValues = [];
-
-for (; line <= numMoneyValues; line++) {
-  moneyValues.push(input[line]);
+const [n, k] = input[line++].split(' ').map(Number);
+const arr = [];
+for (let i = 0; i < n; i++) {
+  arr.push(+input[line++]);
 }
 
-moneyValues.sort((a, b) => b - a);
+arr.sort((a, b) => b - a);
 
-let restMoney = toMakeValue;
-let answer = 0;
+let rest = k;
+let ans = 0;
+let idx = 0;
+while (rest > 0) {
+  ans += Math.floor(rest / arr[idx]);
+  rest = rest % arr[idx];
+  ++idx;
+}
 
-moneyValues.forEach((value) => {
-  if (restMoney === 0) return;
-
-  const num = Math.floor(restMoney / value);
-  if (num > 0) {
-    restMoney -= num * value;
-    answer += num;
-  }
-});
-
-console.log(answer);
+console.log(ans);
