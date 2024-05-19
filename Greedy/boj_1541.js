17e60d8f25cc3efc36faf5ee03f157cdc4add430
zlx454545 <zlx454545@gmail.com>
2024-05-19 18:12:44 +0900

    boj 1541

--- Greedy/boj_1541.js
+++ Greedy/boj_1541.js
@@ -7,13 +7,13 @@ const n = input[line];
 
 const arr = n.split('-');
 
-let ans = 0;
-arr.forEach((el, i) => {
-  const temp = el.split('+').map(Number);
-  const sum = temp.reduce((acc, cur) => acc + cur, 0);
-
-  if (i === 0) return (ans += sum);
-  ans -= sum;
-});
+const ans = arr.reduce((acc, cur, idx) => {
+  const temp = cur
+    .split('+')
+    .map(Number)
+    .reduce((acc, cur) => acc + cur, 0);
+  if (idx === 0) return temp;
+  return acc - temp;
+}, 0);
 
 console.log(ans);
