fa0e3c7d2f27dbfc1a6349a6100cb5eea714f66f
zlx454545 <zlx454545@gmail.com>
2024-05-20 12:21:51 +0900

    boj 2839

--- Greedy/boj_2839.js
+++ Greedy/boj_2839.js
@@ -3,22 +3,55 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 let line = 0;
 
-let n = +input[line++];
-let ans = 0;
+const n = +input[line];
 
-while (n !== 0) {
-  if (n < 3) {
-    ans = -1;
-    break;
+class Q {
+  constructor() {
+    this.items = {};
+    this.head = 0;
+    this.tail = 0;
   }
 
-  if (n % 5 === 0) {
-    ans += n / 5;
-    n = 0;
-  } else {
-    ans += 1;
-    n -= 3;
+  push(item) {
+    this.items[this.tail++] = item;
+  }
+
+  pop() {
+    const rv = this.items[this.head];
+    delete this.items[this.head++];
+
+    return rv;
+  }
+
+  isEmpty() {
+    return this.head === this.tail;
   }
 }
 
-console.log(ans);
+const arr = new Array(5003).fill(0);
+
+const bfs = () => {
+  const queue = new Q();
+
+  queue.push(3);
+  arr[3] = 1;
+  queue.push(5);
+  arr[5] = 1;
+
+  while (!queue.isEmpty()) {
+    const now = queue.pop();
+
+    const distance = [3, 5];
+    for (let d of distance) {
+      if (arr[now + d]) continue;
+      if (now + d > n) continue;
+      queue.push(now + d);
+      arr[now + d] = arr[now] + 1;
+    }
+  }
+};
+
+bfs();
+
+if (!arr[n]) console.log(-1);
+else console.log(arr[n]);
