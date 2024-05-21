dfd209cff71fbf386fddcf1b0d537c57f1d09f66
zlx454545 <zlx454545@gmail.com>
2024-05-22 00:37:40 +0900

    boj 16953

--- Greedy/boj_16953.js
+++ Greedy/boj_16953.js
@@ -4,20 +4,48 @@ let input = fs.readFileSync(filePath).toString().split('\n');
 let line = 0;
 
 let [a, b] = input[line].split(' ').map(Number);
-let ans = 1;
 
-while (a < b) {
-  if (b % 2 === 0) {
-    b /= 2;
-    ++ans;
-  } else if (b % 10 === 1) {
-    b -= 1;
-    b /= 10;
-    ++ans;
-  } else {
-    break;
+class Queue {
+  constructor() {
+    this.items = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  push(item) {
+    this.items[this.tail++] = item;
+  }
+
+  pop() {
+    const rv = this.items[this.head];
+    delete this.items[this.head++];
+
+    return rv;
+  }
+
+  isEmpty() {
+    return this.head === this.tail;
   }
 }
 
-if (a === b) console.log(ans);
-else console.log(-1);
+const arr = new Array(b + 1).fill(0);
+
+const q = new Queue();
+q.push(a);
+arr[a] = 1;
+
+while (!q.isEmpty()) {
+  const now = q.pop();
+  const nexts = [now * 2, now * 10 + 1];
+
+  nexts.forEach((next) => {
+    if (next > b) return;
+    if (arr[next]) return;
+
+    q.push(next);
+    arr[next] = arr[now] + 1;
+  });
+}
+
+if (!arr[b]) console.log(-1);
+else console.log(arr[b]);
