f410bb281895c03db7969e47ec7eba0078ac3820
zlx454545 <zlx454545@gmail.com>
2024-05-24 23:52:34 +0900

    boj 1931

--- Greedy/boj_1931.js
+++ Greedy/boj_1931.js
@@ -6,25 +6,28 @@ let line = 0;
 const n = +input[line++];
 
 const arr = [];
+
 for (let i = 0; i < n; i++) {
-  const [start, end] = input[line++].split(' ').map(Number);
-  arr.push([start, end]);
+  const [a, b] = input[line++].split(' ').map(Number);
+  arr.push([a, b]);
 }
 
 arr.sort((a, b) => {
   if (a[1] === b[1]) {
     return a[0] - b[0];
   }
+
   return a[1] - b[1];
 });
 
-let ans = 0;
-let temp = 0;
-arr.forEach((el) => {
-  if (temp <= el[0]) {
-    ++ans;
-    temp = el[1];
-  }
-});
+let endTime = arr[0][1];
+let ans = 1;
+
+for (let i = 1; i < n; i++) {
+  const [a, b] = arr[i];
+  if (a < endTime) continue;
+  endTime = b;
+  ++ans;
+}
 
 console.log(ans);
